651aa926532e7d02e472f15ccab31fbd35ca2784
Peter Droogmans (attiks) <peter@attiks.com>
2022-11-14 15:02:18 +0100

    feat: Only hide dis-agg button
    Refs: #AR-290

--- html/themes/custom/common_design_subtheme/js/cd-hide-export-button.js
+++ html/themes/custom/common_design_subtheme/js/cd-hide-export-button.js
@@ -11,11 +11,15 @@
 
       var exportButtonWrapper = document.querySelector('.cd-export-button--wrapper');
       var summaryCount = document.querySelector('.source-summary-count');
+      var feedButton = document.querySelector('.CSV-disaggregated-feed');
+      if (feedButton) {
+        feedButton.style.display = 'none';
+      }
 
       if (exportButtonWrapper && summaryCount) {
         var count = parseInt(summaryCount.innerText.split(' ')[0], 10);
-        if (count == 0 || count > 9999) {
-          exportButtonWrapper.remove();
+        if (count > 99) {
+          document.querySelector('.CSV-disaggregated-feed').remove();
           this.removed = true;
           return;
         }
@@ -24,7 +28,7 @@
           window.clearTimeout(this.timerId);
         }
         this.timerId = window.setTimeout(function () {
-          document.querySelector('.cd-export-button--wrapper').style.display = 'auto';
+          document.querySelector('.CSV-disaggregated-feed').style.display = 'inherit';
         }, 500);
       }
     }
